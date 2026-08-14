39e0fb449398cc3fffca5a985570e5354e6ded32
fn1591398-sys <fn1591398@gmail.com>
2026-08-15 00:23:23 +0300

    Create api.ts

---
+++ netlify/functions/api.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import serverless from "serverless-http";
+import { registerOAuthRoutes } from "../../server/_core/oauth";
+import { createContext } from "../../server/_core/context";
+import { appRouter } from "../../server/routers";
+
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+registerOAuthRoutes(app);
+app.use("/api/trpc", createExpressMiddleware({ router: appRouter, createContext }));
+
+export const handler = serverless(app, { basePath: "/.netlify/functions/api" });
